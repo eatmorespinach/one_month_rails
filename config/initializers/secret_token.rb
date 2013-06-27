5e4aec2c688a987ec510b50886a3b7b7377f10e7
Drew Sing <sing.drew@gmail.com>
2013-06-27 19:22:16 -0400

    1st commit. Competed setup and devise gem exercise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OneMonthRails::Application.config.secret_token = 'e63dd208c30f5df3aa42ed6f195575683c7ce9d18f3a67005eeaa147850a282f4cfc4d94a7bc1c6b8b65fbd4d9fcc7f1ab6d1437cc6ee130922090db8a3ba8c5'
